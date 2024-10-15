3324e3aa91d8243c4d0759f10ec0802cc927f51d
pdeogade <pratikdeogade.learning@gmail.com>
2024-10-15 20:26:37 +0530

    weather app- added network cache

---
+++ app/src/main/java/com/example/myweatherapp/common/ConnectionManager.kt
@@ -0,0 +1,28 @@
+package com.example.myweatherapp.common
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import javax.inject.Inject
+
+class ConnectionManager @Inject constructor(private val context: Context) {
+
+    fun isNetworkConnected(): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
+    }
+}
--- app/src/main/java/com/example/myweatherapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/myweatherapp/di/NetworkModule.kt
@@ -1,17 +1,21 @@
 package com.example.myweatherapp.di
 
+import android.app.Application
 import com.example.myweatherapp.BuildConfig
 import com.example.myweatherapp.common.BASE_URL
+import com.example.myweatherapp.common.ConnectionManager
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import com.example.myweatherapp.data.network.WeatherApi
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import javax.inject.Singleton
 
 @Module
@@ -25,8 +29,33 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+    fun provideCache(application: Application): Cache {
+        val cacheSize = 10 * 1024 * 1024 // 10 MB
+        val cacheDir = File(application.cacheDir, "http_cache")
+        return Cache(cacheDir, cacheSize.toLong())
+    }
+
+    @Singleton
+    @Provides
+    fun provideNetworkInfo(application: Application): ConnectionManager {
+        return ConnectionManager(application)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor, cache: Cache, connectionManager: ConnectionManager): OkHttpClient {
         val okHttpClient = OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor { chain ->
+                var request = chain.request()
+                request = if (connectionManager.isNetworkConnected()) { // Add your network condition here
+                    request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+                } else {
+                    request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+                }
+                chain.proceed(request)
+            }
+
         return if (BuildConfig.DEBUG) {
             okHttpClient.addInterceptor(httpLoggingInterceptor).build()
         } else {
@@ -46,5 +75,4 @@ object NetworkModule {
     @Provides
     fun provideWeatherApi(retrofit: Retrofit): WeatherApi = retrofit
         .create(WeatherApi::class.java)
-
 }
